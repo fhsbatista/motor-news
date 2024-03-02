28c27ee625f2674d8aba2ab940d8e7baf8b955ad
Fernando Batista <fernandohsbatista@gmail.com>
2024-03-02 16:34:35 -0300

    ensure /status returns postgre version

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,8 +2,17 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  response.status(200).json({ 
+
+  const databaseQuery = await database.query("SHOW server_version;");
+  const databaseVersion = databaseQuery.rows[0].server_version;
+
+  response.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+      },
+    },
   });
 }
 
