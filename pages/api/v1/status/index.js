113cf3e8fd4bbac5a3660d63ede5f5ce19854013
Fernando Batista <fernandohsbatista@gmail.com>
2024-03-02 17:04:35 -0300

    ensure /status returns database max connections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,14 +3,19 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseQuery = await database.query("SHOW server_version;");
-  const databaseVersion = databaseQuery.rows[0].server_version;
+  const databaseQueryVersion = await database.query("SHOW server_version;");
+  const databaseVersion = databaseQueryVersion.rows[0].server_version;
+
+  const databaseMaxConnsQuery = await database.query("SHOW max_connections;");
+  const databaseMaxConns = databaseMaxConnsQuery.rows[0].max_connections;
+
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersion,
+        max_connections: parseInt(databaseMaxConns),
       },
     },
   });
