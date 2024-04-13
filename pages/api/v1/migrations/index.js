e56bcfd8895e6c1b9fd00b1703261ec9db038e78
Fernando Batista <fernandohsbatista@gmail.com>
2024-04-13 13:51:04 -0300

    refactor /migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,26 +2,26 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
+  const defaultOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dir: join("infra", "migrations"),
+    dryRun: true,
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
   if (request.method === "GET") {
     const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
+      ...defaultOptions,
     });
     response.status(200).json(migrations);
   }
 
   if (request.method === "POST") {
     const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+      ...defaultOptions,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     });
     response.status(200).json(migrations);
   }
